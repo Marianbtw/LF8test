31f3f7fb706ce9b82335759366d484523128f04d
Marian Vonsien <mvo@zyres.com>
2021-09-17 12:09:01 +0200

    created c# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LF8test_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
